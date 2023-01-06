bddf9dfd33540af39df86d936fe830b15d62a95e
Maria Mer <maria.mer@outlook.dk>
2023-01-06 07:42:07 +0100

    created new app

---
+++ src/app.js
@@ -0,0 +1,137 @@
+function formattedDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
+function formattedDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let weatherForecast = document.querySelector("#weather-forecast");
+  let dailyForecast = response.data.daily;
+
+  let forecastHtml = `<div class="row">`;
+
+  dailyForecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        ` <div class="col-2">
+              <div class="weather-forecast-day">
+                <span>${formattedDay(forecastDay.time)}</span>
+              </div>
+              <img
+                src="${forecastDay.condition.icon_url}"
+                alt="${forecastDay.condition.icon}"
+                width="50"
+              />
+              <div class="weather-forecast-temp">
+                <span class="forecast-temp-max">${Math.round(
+                  forecastDay.temperature.maximum
+                )}°</span>
+                </br>
+                <span class="forecast-temp-min">${Math.round(
+                  forecastDay.temperature.minimum
+                )}°</span>
+              </div>
+            </div>`;
+    }
+  });
+
+  forecastHtml = forecastHtml + `</div>`;
+  weatherForecast.innerHTML = forecastHtml;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "at731cf267b4abccf566243f6ceo3306";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let date = document.querySelector("#date");
+  let mainIcon = document.querySelector("#main-weather-icon");
+
+  celsiusTemp = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+  city.innerHTML = response.data.city;
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = response.data.temperature.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = formattedDate(response.data.time * 1000);
+  mainIcon.setAttribute("src", response.data.condition.icon_url);
+  mainIcon.setAttribute("alt", response.data.condition.icon);
+
+  getForecast(response.data.coordinates);
+}
+
+function search(city) {
+  let apiKey = "at731cf267b4abccf566243f6ceo3306";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-input");
+  search(cityInput.value);
+}
+
+const successCallback = (position) => {
+  console.log(position);
+};
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+
+function searchLocation(position) {
+  let apiKey = "at731cf267b4abccf566243f6ceo3306";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(displayTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+search("Athens");
